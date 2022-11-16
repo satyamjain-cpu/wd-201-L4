aafa874d1afc28f306b11415855599509beac186
satyamjain-cpu <113338521+satyamjain-cpu@users.noreply.github.com>
2022-11-16 16:05:53 +0530

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,55 @@
+const todoList = () => {
+  const all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+   
+    return all.filter(
+      (item) => item.dueDate < new Date().toLocaleDateString("en-CA")
+    );
+  };
+
+  const dueToday = () => {
+   
+    return all.filter(
+      (item) => item.dueDate == new Date().toLocaleDateString("en-CA")
+    );
+  };
+
+  const dueLater = () => {
+ 
+    return all.filter(
+      (item) => item.dueDate > new Date().toLocaleDateString("en-CA")
+    );
+  };
+
+  const toDisplayableList = (list) => {
+   
+
+    return list
+      .map(
+        (todo) =>
+          `${todo.completed ? "[x]" : "[ ]"} ${todo.title} ${
+            todo.dueDate == today ? "" : todo.dueDate
+          }`
+      )
+      .join("\n").trim();
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+// [do not change anything below this line.] #
+module.exports = todoList;z
