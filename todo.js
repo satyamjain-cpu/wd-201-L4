429f2513695c09470832ddfba44ee00f389a0a3b
satyamjain-cpu <113338521+satyamjain-cpu@users.noreply.github.com>
2022-11-20 01:09:41 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,4 +1,5 @@
 const todoList = () => {
+  let today;
   const all = [];
   const add = (todoItem) => {
     all.push(todoItem);
@@ -8,28 +9,50 @@ const todoList = () => {
   };
 
   const overdue = () => {
-   
+    // Write the date check condition here and return the array of overdue items accordingly.
+    // FILL YOUR CODE HERE
+    // ..
+    // ..
+    // ..
     return all.filter(
       (item) => item.dueDate < new Date().toLocaleDateString("en-CA")
     );
   };
 
   const dueToday = () => {
-   
+    // Write the date check condition here and return the array of todo items that are due today accordingly.
+    // FILL YOUR CODE HERE
+    // ..
+    // ..
+    // ..
+    //   const returnValue = () =>{
+    //     all.filter((item)=>item.dueDate===new Date().toLocaleDateString("en-CA"));
+    //   }
+    //   return returnValue;
+    //return all.filter((item)=> item.dueDate==new Date().toLocaleDateString("en-CA"));
     return all.filter(
       (item) => item.dueDate == new Date().toLocaleDateString("en-CA")
     );
   };
 
   const dueLater = () => {
- 
+    // Write the date check condition here and return the array of todo items that are due later accordingly.
+    // FILL YOUR CODE HERE
+    // ..
+    // ..
+    // ..
     return all.filter(
       (item) => item.dueDate > new Date().toLocaleDateString("en-CA")
     );
   };
 
   const toDisplayableList = (list) => {
-   
+    // Format the To-Do list here, and return the output string as per the format given above.
+    // FILL YOUR CODE HERE
+    // ..
+    // ..
+    // ..
+    // return OUTPUT_STRING
 
     return list
       .map(
@@ -38,7 +61,8 @@ const todoList = () => {
             todo.dueDate == today ? "" : todo.dueDate
           }`
       )
-      .join("\n").trim();
+      .join("\n")
+      .trim();
   };
 
   return {
@@ -51,5 +75,8 @@ const todoList = () => {
     toDisplayableList,
   };
 };
-// [do not change anything below this line.] #
-module.exports = todoList;z
+
+// ####################################### #
+// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
+// ####################################### #
+module.exports = todoList;
